d21c328e3bad63583c16a2810fc0ad3d531f6891
bendoan98 <bendoan@cs.washington.edu>
2026-03-15 13:21:05 -0700

    Add RTL/jsdom testing setup

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    setupFiles: ['./src/test/setup.js'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'json-summary'],
